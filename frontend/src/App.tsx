5bb3e6a43fd88ff05e7fe3c0f8fa5182d6ca7d09
Rafael Vieira dos Santos <rafaelvieirasantos2006@hotmail.com>
2021-05-05 15:08:50 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import DataTable from './components/DataTable/index';
 
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá Mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá Mundo!</h1>
+        
+      <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/rafaelrok" target="_blank" rel="noreferrer">Rafael Vieira</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                Evento promovido pela escola DevSuperior:
+                <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
